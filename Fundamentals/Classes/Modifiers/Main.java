f81b5fec974a5bc5da3016a423272153295d0315
Berkantrkgl <e190501030@stud.tau.edu.tr>
2023-03-18 13:53:45 +0300

    Add a empty Classe/Modifiers/Main.java file

---
+++ Fundamentals/Classes/Modifiers/Main.java
@@ -0,0 +1,12 @@
+package Fundamentals.Classes.Modifiers;
+
+public class Main {
+    /*
+     * The public keyword is an access modifier, 
+     * meaning that it is used to set the access level for classes, 
+     * attributes, methods and constructors.
+     * We divide modifiers into two groups
+     */
+
+    
+}
